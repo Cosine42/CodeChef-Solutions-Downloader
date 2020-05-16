cf8112fcb49116303c1b45abc7932a801ad493f5
Tithi Ghosh <65277727+Cosine42@users.noreply.github.com>
2020-05-16 22:27:06 +0530

    Update cc_scraper.py

--- cc_scraper.py
+++ cc_scraper.py
@@ -3,8 +3,8 @@ import os
 from bs4 import BeautifulSoup as bs
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-
-username='cosine1509'
+print("enter username")
+username=input()
 # creating the download folder
 os.makedirs(username)
 
